431ed2107574486f760715a8f15c62b53e8d2d51
Hebzi <dumaradek@gmail.com>
2021-07-10 14:52:54 +0200

    Fix modes buttons
    Now buttons work properly. I also added a confirmation dialogue for each mode.

--- temp_regulator/static/temp_regulator/js/temp_buttons.js
+++ temp_regulator/static/temp_regulator/js/temp_buttons.js
@@ -1,5 +1,5 @@
 function set_temperature(temp, sign) {
-    return confirm('You\'re going to set temperature to ' + temp + sign + 'C. Are you sure?' )
+    return confirm('You\'re going to set temperature to ' + temp + sign + 'C. Are you sure?')
 }
 
 // $(document).ready(function () {
