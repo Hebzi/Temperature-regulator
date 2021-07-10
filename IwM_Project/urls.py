3c657c57c274ed6932ceca5ffe1cfa05b9012b44
Hebzi <dumaradek@gmail.com>
2021-07-10 14:21:57 +0200

    Add login/logout page and user authentication

--- IwM_Project/urls.py
+++ IwM_Project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as u_views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('temp_regulator.urls')),
     path('register/', u_views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
